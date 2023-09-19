6030a738fd93e5809c63349bd778b76ce6a19743
Carolina Osei Alvarez <oseic1@bfh.ch>
2023-09-19 07:55:02 +0200

    finish

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -30,7 +30,7 @@ const singleWord = {
 //staggerChildren mit 0.08 hat zur Folge, dass alle Kinder verzögert geladen werden. Da der Block unter Motion.span von quote erbt braucht es kein initial und animate unter variants= {singleWord}
 const AnimatedText =({text, className=""}) => {
 		return (
-			<div className='w-full mx-auto py-2 . flex items-center justify-center text-center overflow-hidden'>
+			<div className='w-full mx-auto py-2 . flex items-center justify-center text-center overflow-hidden sm:py-0'>
 				<motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className}`}
 				variants={quote}
 				initial="initial"
