a5a8a1c9eecd77bddff859bf9b91040e1be13851
Carolina Osei Alvarez <oseic1@bfh.ch>
2023-10-04 22:19:49 +0200

    mail in contact  button changed

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -10,7 +10,7 @@ const HireMe = () => {
 			<div className='w-60 h-auto flex items-center justify-center relative md:w-24'>
 				<CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
 
-				<Link href="mailto:abc@gmail.com" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+				<Link href="mailto:kwasi.alvarez@hotmail.com" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
 				 bg-dark text-light shadow-md border border-solid border-dark w-24 h-24 rounded-full 
 
 				 hover:bg-light hover:text-dark
